2ee73065c81c07a0bf2e3ef3becbfb81a9791bc6
KingDonggyu <dgkim69166916@gmail.com>
2023-01-10 17:45:29 +0900

    feat(#1): 전역 스타일 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Global } from '@emotion/react';
+import globalStyle from '../styles/global';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global styles={globalStyle} />
+      <Component {...pageProps} />
+    </>
+  );
 }
