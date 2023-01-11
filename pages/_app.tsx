404b7fab2abc066c1f7f75687ebde0fec2953827
KingDonggyu <dgkim69166916@gmail.com>
2023-01-11 18:01:33 +0900

    feat(#1): theme style 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app';
-import { Global } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import globalStyle from '@/styles/global';
+import theme from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ styles/theme.ts
@@ -0,0 +1,19 @@
+import { Theme } from '@emotion/react';
+
+const lightModeColors = {
+  PRIMARY: '#5AB890',
+  TEXT: '#3E3E3E',
+  BACKGROUND: '#FCFCFC',
+};
+
+const darkModeColors = {
+  PRIMARY: '#5AB890',
+  TEXT: '#FCFCFC',
+  BACKGROUND: '#3E3E3E',
+};
+
+const theme: Theme = {
+  colors: lightModeColors,
+};
+
+export default theme;
