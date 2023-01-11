01d8060dd6520a675ab5c317d09500bcaae64e60
KingDonggyu <dgkim69166916@gmail.com>
2023-01-11 18:28:46 +0900

    feat(#3): 로고 atom 구현

---
+++ components/atoms/Logo/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import logoLightSrc from '@/public/images/logo-light.svg';
+import logoDarkSrc from '@/public/images/logo-dark.svg';
+
+interface LogoProps {
+  isDark: boolean;
+}
+
+const Logo = ({ isDark }: LogoProps) => (
+  <Image src={isDark ? logoDarkSrc : logoLightSrc} alt="로고 이미지" />
+);
+
+export default Logo;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,11 @@ import { Global, ThemeProvider } from '@emotion/react';
 import globalStyle from '@/styles/global';
 import theme from '@/styles/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Global styles={globalStyle} />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <Global styles={globalStyle} />
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
+
+export default App;
