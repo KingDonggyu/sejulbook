07665f9c7c90439e459325c2dc62a4130a1486c0
KingDonggyu <dgkim69166916@gmail.com>
2023-01-28 18:47:35 +0900

    feat(#15): debounce Hook 구현

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+const useDebounce = (value: unknown, delay = 100) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
