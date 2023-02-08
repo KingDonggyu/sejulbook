2186bcecf762cad95b31972f9a1986d469d0a84d
KingDonggyu <dgkim69166916@gmail.com>
2023-02-09 00:31:18 +0900

    refactror(#15): Library Template 분리

--- src/pages/libraries/[id].tsx
+++ src/pages/libraries/[id].tsx
@@ -1,23 +1,5 @@
-import DocumentTitle from '@/components/atoms/DocumentTitle';
-import ProfileEditButton from '@/components/molecules/ProfileEditButton';
-import Profile from '@/components/organisms/Profile';
-import Bookshelf from '@/components/organisms/Bookshelf';
-import BookReivewSort from '@/components/organisms/BookReivewSort';
-import * as s from './style';
+import LibraryTemplate from '@/components/templates/LibraryTemplate';
 
-const LibraryPage = () => (
-  <s.Wrapper>
-    <DocumentTitle title="김동규의 서재" />
-    <s.TopSectionWrapper>
-      <Profile />
-      <s.ButtonWrapper>
-        <ProfileEditButton />
-        <BookReivewSort />
-      </s.ButtonWrapper>
-    </s.TopSectionWrapper>
-    <s.Divider />
-    <Bookshelf />
-  </s.Wrapper>
-);
+const LibraryPage = () => <LibraryTemplate />;
 
 export default LibraryPage;
