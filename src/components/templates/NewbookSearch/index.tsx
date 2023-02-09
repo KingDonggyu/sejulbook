b6494cae8c0837563a07079b2b1c8a9d71cf33cb
KingDonggyu <dgkim69166916@gmail.com>
2023-02-09 17:50:55 +0900

    refactror(#15): NewbookSearch Template 하위 컴포넌트 주입

--- src/components/templates/NewbookSearch/index.tsx
+++ src/components/templates/NewbookSearch/index.tsx
@@ -1,6 +1,5 @@
+import { ReactNode } from 'react';
 import styled from '@emotion/styled';
-import DocumentTitle from '@/components/atoms/DocumentTitle';
-import BookSearchBar from '@/components/organisms/BookSearchBar';
 
 const Wrapper = styled.div`
   margin: auto;
@@ -8,11 +7,12 @@ const Wrapper = styled.div`
   max-width: ${({ theme }) => theme.MAX_WIDTH.CONTENT};
 `;
 
-const NewbookSearch = () => (
-  <Wrapper>
-    <DocumentTitle title="독후감 쓰기" />
-    <BookSearchBar placeholder="책을 선택해주세요." />
-  </Wrapper>
+interface NewbookSearchProps {
+  bookSearchBar: ReactNode;
+}
+
+const NewbookSearch = ({ bookSearchBar }: NewbookSearchProps) => (
+  <Wrapper>{bookSearchBar}</Wrapper>
 );
 
 export default NewbookSearch;
--- src/pages/newbook/search.tsx
+++ src/pages/newbook/search.tsx
@@ -1,9 +1,14 @@
 import NewbookSearch from '@/components/templates/NewbookSearch';
+import DocumentTitle from '@/components/atoms/DocumentTitle';
+import BookSearchBar from '@/components/organisms/BookSearchBar';
 import { NewbookProvider } from '@/contexts/newbookContext';
 
 const NewbookSearchPage = () => (
   <NewbookProvider>
-    <NewbookSearch />
+    <DocumentTitle title="책 선택" />
+    <NewbookSearch
+      bookSearchBar={<BookSearchBar placeholder="책을 선택해주세요." />}
+    />
   </NewbookProvider>
 );
 
