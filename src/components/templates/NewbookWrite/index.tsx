88b7185b4d44bfcfd55e0c754dea6b0a4a420d6e
KingDonggyu <dgkim69166916@gmail.com>
2023-02-09 17:58:59 +0900

    refactror(#15): NewbookWrite Template 하위 컴포넌트 주입

--- src/components/templates/NewbookWrite/index.tsx
+++ src/components/templates/NewbookWrite/index.tsx
@@ -1,14 +1,22 @@
-import { useEffect } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { useLayoutContext } from '@/contexts/layoutContext';
-import Button from '@/components/atoms/Button';
-import SejulTextarea from '@/components/organisms/SejulTextarea';
-import ContentTextarea from '@/components/organisms/ContentTextarea';
-import { useNewbookContext } from '@/contexts/newbookContext';
-import { ButtonVariant, ColorVariant } from '@/constants';
 import * as s from './style';
 
-const NewbookWrite = () => {
-  const { newbook } = useNewbookContext();
+interface NewbookWriteProps {
+  bookName: string;
+  sejulTextarea: ReactNode;
+  contentTextarea: ReactNode;
+  publishButton: ReactNode;
+  draftSaveButton: ReactNode;
+}
+
+const NewbookWrite = ({
+  bookName,
+  sejulTextarea,
+  contentTextarea,
+  publishButton,
+  draftSaveButton,
+}: NewbookWriteProps) => {
   const {
     showHeaderBar,
     hideHeaderBar,
@@ -33,15 +41,13 @@ const NewbookWrite = () => {
   return (
     <>
       <s.ButtonWrapper>
-        <Button variant={ButtonVariant.OUTLINED}>임시 저장</Button>
-        <Button variant={ButtonVariant.OUTLINED} color={ColorVariant.PRIMARY}>
-          발행
-        </Button>
+        {draftSaveButton}
+        {publishButton}
       </s.ButtonWrapper>
       <s.Wrapper>
-        <s.BookName>브레이킹 루틴</s.BookName>
-        <SejulTextarea />
-        <ContentTextarea />
+        <s.BookName>{bookName}</s.BookName>
+        {sejulTextarea}
+        {contentTextarea}
       </s.Wrapper>
     </>
   );
--- src/pages/newbook/write.tsx
+++ src/pages/newbook/write.tsx
@@ -1,9 +1,27 @@
 import NewbookWrite from '@/components/templates/NewbookWrite';
+import DocumentTitle from '@/components/atoms/DocumentTitle';
+import Button from '@/components/atoms/Button';
+import SejulTextarea from '@/components/organisms/SejulTextarea';
+import ContentTextarea from '@/components/organisms/ContentTextarea';
+import { ButtonVariant, ColorVariant } from '@/constants';
 import { NewbookProvider } from '@/contexts/newbookContext';
 
 const NewbookWritePage = () => (
   <NewbookProvider>
-    <NewbookWrite />
+    <DocumentTitle title="독후감 쓰기" />
+    <NewbookWrite
+      bookName="브레이킹 루틴"
+      sejulTextarea={<SejulTextarea />}
+      contentTextarea={<ContentTextarea />}
+      publishButton={
+        <Button variant={ButtonVariant.OUTLINED} color={ColorVariant.PRIMARY}>
+          발행
+        </Button>
+      }
+      draftSaveButton={
+        <Button variant={ButtonVariant.OUTLINED}>임시 저장</Button>
+      }
+    />
   </NewbookProvider>
 );
 
