7c0b1123250f8d1deb0259f96d8587ee2c719b5a
KingDonggyu <dgkim69166916@gmail.com>
2023-03-02 19:50:12 +0900

    refactor(#30): Query 인터페이스 내 queryFn 반환 타입 변경

--- src/types/query.d.ts
+++ src/types/query.d.ts
@@ -2,7 +2,7 @@ import { QueryKey, UseQueryOptions } from '@tanstack/react-query';
 
 interface Query {
   queryKey: QueryKey;
-  queryFn: () => void;
+  queryFn: () => unknown;
   options?: UseQueryOptions;
 }
 
