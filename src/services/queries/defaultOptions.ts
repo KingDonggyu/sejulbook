96e93b25c1e341d1c40a6913dd2afaf3b51ae8cf
KingDonggyu <dgkim69166916@gmail.com>
2023-03-03 16:42:48 +0900

    fix(#43): React Query defaultOptions 내 refetchOnMount: false 추가

---
+++ src/services/queries/defaultOptions.ts
@@ -0,0 +1,9 @@
+import { DefaultOptions } from '@tanstack/react-query';
+
+const defaultOptions: DefaultOptions = {
+  queries: {
+    refetchOnMount: false,
+  },
+};
+
+export default defaultOptions;
