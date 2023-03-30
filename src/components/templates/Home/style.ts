5ec985493885305d9b8e7d017dd93a717f472a63
KingDonggyu <dgkim69166916@gmail.com>
2023-03-30 18:55:22 +0900

    hofix: home page 내 배너글 모바일 반응형 UI 개선

--- src/components/templates/Home/style.ts
+++ src/components/templates/Home/style.ts
@@ -18,7 +18,7 @@ export const Banner = styled.div`
   background: ${({ theme }) => theme.COLOR.PRIMARY};
   font-family: ${({ theme }) => theme.FONT_FAMILY.nanumMyeongjo};
   @media screen and (max-width: ${({ theme }) => theme.MAX_WIDTH.MOBILE}) {
-    padding: 30px 0 30px 15%;
+    padding: 30px 0 30px 12%;
     padding-top: ${({ theme }) => theme.HEADERBAR_HEIGHT + 30}px;
   }
 `;
