8030ab50bf7ee9a03dc64aa45999aaf877e6042a
KingDonggyu <dgkim69166916@gmail.com>
2023-05-18 15:54:19 +0900

    refactor: 세 줄 독후감 렌더링 여부를 CSS hover로 제어하도록 변경

--- src/components/organisms/SejulThumbnail/style.ts
+++ src/components/organisms/SejulThumbnail/style.ts
@@ -1,25 +1,37 @@
 import styled from '@emotion/styled';
 import { THUMBNAIL_Z_INDEX } from '@/constants/zIndex';
 
-export const ThumnailWrapper = styled.div`
+export const ThumnailWrapper = styled.div<{ isGrayscale: boolean }>`
   cursor: pointer;
   position: relative;
   overflow: hidden;
   z-index: ${THUMBNAIL_Z_INDEX};
   font-family: ${({ theme }) => theme.FONT_FAMILY.nanumMyeongjo};
 
+  img {
+    transition: all 1s;
+    filter: ${({ isGrayscale }) => (isGrayscale ? 'brightness(0.5)' : 'none')};
+  }
+
   @media (hover: hover) and (pointer: fine) {
     :hover {
       img {
+        filter: brightness(0.3) !important;
         transform: scale(1.07);
         transition: 0.5s;
+      }
+      & > p {
+        opacity: 1;
+      }
+      & > div {
+        opacity: 0;
       }
     }
   }
 `;
 
 export const SejulBookReview = styled.p<{ isLarge: boolean }>`
-  line-height: 1.7;
+  line-height: 2;
   overflow-y: auto;
   overflow-x: hidden;
   white-space: pre-line;
@@ -30,7 +42,12 @@ export const SejulBookReview = styled.p<{ isLarge: boolean }>`
   padding: 10px;
   padding-bottom: 30px;
   height: 100%;
+  opacity: 0;
   color: ${({ theme }) => theme.COLOR.WHITE};
   font-size: ${({ theme, isLarge }) =>
     isLarge ? theme.FONT_SIZE.MEDIUM : theme.FONT_SIZE.SMALL};
+`;
+
+export const DefaultContent = styled.div`
+  opacity: 1;
 `;
