981ba6cdcf02d00c7d69784edb1c13e25b652066
KingDonggyu <dgkim69166916@gmail.com>
2023-06-18 16:19:00 +0900

    refactor(#180): User 데이터 접근 인터페이스 repository 생성

---
+++ src/repository/api/UserRepository.ts
@@ -0,0 +1,47 @@
+import UserService, {
+  Id,
+  CreateUserRequestDTO,
+  UpdateUserRequestDTO,
+  FindPagedUserRequestDTO,
+} from 'server/services/user/user.service';
+import HttpClient from './HttpClient';
+
+class UserRepository extends HttpClient {
+  private service = new UserService();
+
+  private baseUrl = '/user';
+
+  async signUp(user: CreateUserRequestDTO) {
+    this.service.create(user);
+  }
+
+  async update(user: UpdateUserRequestDTO) {
+    this.axiosInstance.put(`${this.baseUrl}/${user.id}`, user);
+  }
+
+  async delete(userId: Id) {
+    this.axiosInstance.delete(`${this.baseUrl}/${userId}`);
+  }
+
+  get(userId: Id) {
+    return this.service.findById(userId);
+  }
+
+  search(query: string) {
+    return this.service.findAllByNamePrefix(query);
+  }
+
+  getIds() {
+    return this.service.findAllId();
+  }
+
+  getPagedFollowers(request: FindPagedUserRequestDTO) {
+    return this.service.findPagedFollowers(request);
+  }
+
+  getPagedFollowings(request: FindPagedUserRequestDTO) {
+    return this.service.findPagedFollowings(request);
+  }
+}
+
+export default UserRepository;
