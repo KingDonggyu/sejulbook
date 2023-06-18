3058d4c21e46dff8acd21cd823b6529742537071
KingDonggyu <dgkim69166916@gmail.com>
2023-06-18 16:19:59 +0900

    refactor(#180): Comment 데이터 접근 인터페이스 repository 생성

---
+++ src/repository/api/CommentRepository.ts
@@ -0,0 +1,52 @@
+import CommentService, {
+  Id,
+  BookReviewId,
+  CommenterId,
+  Content,
+  CreateRequestDTO,
+} from 'server/services/comment/comment.service';
+import HttpClient from './HttpClient';
+
+interface DeleteRequest {
+  id: Id;
+  bookReviewId: BookReviewId;
+  commenterId: CommenterId;
+}
+
+interface UpdateRequest extends DeleteRequest {
+  content: Content;
+}
+
+class CommentRepository extends HttpClient {
+  private service = new CommentService();
+
+  private baseUrl = '/comment';
+
+  async set({ bookReviewId, commenterId, content }: CreateRequestDTO) {
+    this.axiosInstance.post(
+      `${this.baseUrl}/${bookReviewId}`,
+      { content },
+      { params: { commenterId } },
+    );
+  }
+
+  async update({ id, bookReviewId, commenterId, content }: UpdateRequest) {
+    this.axiosInstance.put(
+      `${this.baseUrl}/${bookReviewId}`,
+      { id, content },
+      { params: { commenterId } },
+    );
+  }
+
+  async delete({ id, bookReviewId, commenterId }: DeleteRequest) {
+    this.axiosInstance.delete(`${this.baseUrl}/${bookReviewId}`, {
+      params: { id, commenterId },
+    });
+  }
+
+  get(bookReviewId: BookReviewId) {
+    return this.service.findAllByBookReview(bookReviewId);
+  }
+}
+
+export default CommentRepository;
