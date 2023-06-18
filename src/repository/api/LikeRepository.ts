caa3865d8164d6dca73a38c4f9506386d70394a3
KingDonggyu <dgkim69166916@gmail.com>
2023-06-18 16:19:25 +0900

    refactor(#180): Like 데이터 접근 인터페이스 repository 생성

---
+++ src/repository/api/LikeRepository.ts
@@ -0,0 +1,32 @@
+import LikeService, {
+  LikeDefaultRequestDTO,
+} from 'server/services/like/like.service';
+import HttpClient from './HttpClient';
+
+class LikeRepository extends HttpClient {
+  private service = new LikeService();
+
+  private baseUrl = '/like';
+
+  async like({ bookReviewId, likerId }: LikeDefaultRequestDTO) {
+    this.axiosInstance.post(
+      `${this.baseUrl}/${bookReviewId}`,
+      {},
+      {
+        params: { likerId },
+      },
+    );
+  }
+
+  async unlike({ bookReviewId, likerId }: LikeDefaultRequestDTO) {
+    this.axiosInstance.delete(`${this.baseUrl}/${bookReviewId}`, {
+      params: { likerId },
+    });
+  }
+
+  has({ bookReviewId, likerId }: LikeDefaultRequestDTO) {
+    return this.service.has({ bookReviewId, likerId });
+  }
+}
+
+export default LikeRepository;
