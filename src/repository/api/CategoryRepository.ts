1111492efe7e0d7353b6649f84d85f09f36e8d2c
KingDonggyu <dgkim69166916@gmail.com>
2023-06-18 16:20:12 +0900

    refactor(#180): Category 데이터 접근 인터페이스 repository 생성

---
+++ src/repository/api/CategoryRepository.ts
@@ -0,0 +1,11 @@
+import CategoryService from 'server/services/category/category.service';
+
+class CategoryRepository {
+  private service = new CategoryService();
+
+  get() {
+    return this.service.findAll();
+  }
+}
+
+export default CategoryRepository;
